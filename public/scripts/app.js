e6790d8db38bf1cd7db8dda4de9177524e024d6d
Rachel Gould <rachegould@gmail.com>
2019-02-20 21:48:54 +0000

    Renders all the tweets in hardcoded list with proper styling

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,24 +1,87 @@
 $(document).ready(function() {
-    
-    // Make tweets darker when you mouse over them
-    $('.tweets_entry').hover(enterTweet, exitTweet);
-        function enterTweet() {
-            const $tweet = $(this);
-            // Add hover class to all the elements in the greyed heading for the tweet entry (avatar, full name, username).
-            $tweet.children('.tweets_header').children().addClass('hover');
-            // border around whole thing
-            $tweet.addClass('hover');
-            // The second div that contains buttons
-            $tweet.children('.tweets_buttons').addClass('hover');
+
+    const data = [
+        {
+          "user": {
+            "name": "Newton",
+            "avatars": {
+              "small":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_50.png",
+              "regular": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188.png",
+              "large":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_200.png"
+            },
+            "handle": "@SirIsaac"
+          },
+          "content": {
+            "text": "If I have seen further it is by standing on the shoulders of giants"
+          },
+          "created_at": 1461116232227
+        },
+        {
+          "user": {
+            "name": "Descartes",
+            "avatars": {
+              "small":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_50.png",
+              "regular": "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc.png",
+              "large":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_200.png"
+            },
+            "handle": "@rd" },
+          "content": {
+            "text": "Je pense , donc je suis"
+          },
+          "created_at": 1461113959088
+        },
+        {
+          "user": {
+            "name": "Johann von Goethe",
+            "avatars": {
+              "small":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_50.png",
+              "regular": "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1.png",
+              "large":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_200.png"
+            },
+            "handle": "@johann49"
+          },
+          "content": {
+            "text": "Es ist nichts schrecklicher als eine tätige Unwissenheit."
+          },
+          "created_at": 1461113796368
         }
-        function exitTweet() {
-            const $tweet = $(this);
-            // Remove hover class to all the elements in the greyed heading for the tweet entry (avatar, full name, username).
-            $tweet.children('.tweets_header').children().removeClass('hover');
-            // border around whole thing
-            $tweet.removeClass('hover');
-            // The second div that contains buttons
-            $tweet.children('.tweets_buttons').removeClass('hover');
+      ];
+
+    function renderTweets(tweets) {
+        for (let tweet in tweets) {
+            $('#tweets-container').append(createTweetElement(tweets[tweet]));
         }
-    //----------------------------------------
+    }
+        
+    function createTweetElement(data) {
+        let daysSince = function(date) {
+            let days = 0; // FIX THIS!
+            return `${days} days ago`;
+        }
+        let $article = $('<article>').addClass('tweets_entry');
+        // Everything is nested inside the article:
+        let $header = $('<div>').addClass('tweets_header');
+        // Elements nested inside the header of the tweet:
+        let $avatar = $(`<img src="${data.user.avatars.regular}" alt="${data.user.name} avatar">`);
+        let $fullName = $('<h2>').text(data.user.name);
+        let $userName = $('<h3>').text(data.user.handle);
+        // Siblings of the header:
+        let $content = $('<p>').addClass('tweets_content').text(data.content.text);
+        let $date = $('<p>').addClass('tweets_date').text(daysSince(data['created_at']));
+        let $buttonContainer = $('<div>').addClass('tweets_buttons');
+        // Inside the button container:
+        let $button1 = $('<img src="https://vanillicon.com/v2/d38210281760fba56ba70f58c0065f7a.svg">');
+        let $button2 = $('<img src="https://vanillicon.com/v2/d38210281760fba56ba70f58c0065f7a.svg">');
+        let $button3 = $('<img src="https://vanillicon.com/v2/d38210281760fba56ba70f58c0065f7a.svg">');
+        let $tweet = $($article)
+                    .append($($header)
+                        .append($($avatar), $($fullName), $($userName)))
+                    .append($($content))
+                    .append('<hr>')
+                    .append($($date))
+                    .append($($buttonContainer).append($($button1), $($button2), $($button3)));
+        return $tweet;
+    }
+      
+    renderTweets(data);
   });
