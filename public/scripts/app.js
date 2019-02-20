b3132dc7d0aaffcaee0acfaf4446e12d5b5f59bb
Rachel Gould <rachegould@gmail.com>
2019-02-20 04:57:30 +0000

    Hover over hardcoded tweet changes color and displays placeholder buttons

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,6 +1,24 @@
-/*
- * Client-side JS logic goes here
- * jQuery is already loaded
- * Reminder: Use (and do all your DOM work in) jQuery's document ready function
- */
-
+$(document).ready(function() {
+    
+    // Make tweets darker when you mouse over them
+    $('.tweets_entry').hover(enterTweet, exitTweet);
+        function enterTweet() {
+            const $tweet = $(this);
+            // Add hover class to all the elements in the greyed heading for the tweet entry (avatar, full name, username).
+            $tweet.children('.tweets_header').children().addClass('hover');
+            // border around whole thing
+            $tweet.addClass('hover');
+            // The second div that contains buttons
+            $tweet.children('.tweets_buttons').addClass('hover');
+        }
+        function exitTweet() {
+            const $tweet = $(this);
+            // Remove hover class to all the elements in the greyed heading for the tweet entry (avatar, full name, username).
+            $tweet.children('.tweets_header').children().removeClass('hover');
+            // border around whole thing
+            $tweet.removeClass('hover');
+            // The second div that contains buttons
+            $tweet.children('.tweets_buttons').removeClass('hover');
+        }
+    //----------------------------------------
+  });
